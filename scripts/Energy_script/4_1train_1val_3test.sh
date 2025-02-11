a90c6da05e9752c645f984750aac3f7cbdfc617c
ayushrajdahal <ayushrajdahal@gmail.com>
2025-02-10 19:27:43 -0600

    Add script and data for exp 3 & 4

--- scripts/Energy_script/4_1train_1val_3test.sh
+++ scripts/Energy_script/4_1train_1val_3test.sh
@@ -4,7 +4,7 @@ python3 -u run.py \
   --is_training 1 \
   --root_path ./dataset/Energy/ \
   --data_path load_forecasting.csv \
-  --model_id 3_2train_1val_2test \
+  --model_id 4_1train_1val_3test \
   --model Autoformer \
   --data Energy \
   --features M \
