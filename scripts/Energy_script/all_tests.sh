cdf2d9bdcaf50bf62666c4264c0913d9991c24e5
ayushrajdahal <ayushrajdahal@gmail.com>
2025-02-12 03:31:59 -0600

    Copy all the updated test scripts into a all_tests.sh.

--- scripts/Energy_script/all_tests.sh
+++ scripts/Energy_script/all_tests.sh
@@ -24,6 +24,27 @@ python3 -u run.py \
   --is_training 1 \
   --root_path ./dataset/Energy/ \
   --data_path load_forecasting.csv \
+  --model_id 2_2_sincos \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1 \
+  --embed sincos
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
   --model_id 2_4_48seqlen \
   --model Autoformer \
   --data Energy \
@@ -59,4 +80,253 @@ python3 -u run.py \
   --c_out 16 \
   --des 'Exp' \
   --itr 1
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 2_5_24labellen \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 24 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 2_5_72labellen \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 72 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 2_6_12predlen \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 12 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 2_6_48predlen \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 48 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 2_6_72predlen \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 72 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 2_7_1encoder \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 1 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 2_7_3encoders \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 3 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 2_8_2decoders \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 2 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 2_8_3decoders \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 3 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 3_2train_1val_2test \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1 \
+  --train_yrs 2 \
+  --val_yrs 1 \
+  --test_yrs 2
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 4_1train_1val_3test \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1 \
+  --train_yrs 1 \
+  --val_yrs 1 \
+  --test_yrs 3
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 4_1train_1val_3test \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1 \
+  --train_yrs 1 \
+  --val_yrs 1 \
+  --test_yrs 3
 
