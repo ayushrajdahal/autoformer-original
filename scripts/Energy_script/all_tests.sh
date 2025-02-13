882930ca7933dcdf42ecbe32d64d96e6d010816e
ayushrajdahal <ayushrajdahal@gmail.com>
2025-02-13 07:23:51 -0600

    Test out diff c values, comment sin cos script.

--- scripts/Energy_script/all_tests.sh
+++ scripts/Energy_script/all_tests.sh
@@ -20,26 +20,26 @@ python3 -u run.py \
   --des 'Exp' \
   --itr 1
 
-python3 -u run.py \
-  --is_training 1 \
-  --root_path ./dataset/Energy/ \
-  --data_path load_forecasting.csv \
-  --model_id 2_2_sincos \
-  --model Autoformer \
-  --data Energy \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 24 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 16 \
-  --dec_in 16 \
-  --c_out 16 \
-  --des 'Exp' \
-  --itr 1 \
-  --embed sincos
+# python3 -u run.py \
+#   --is_training 1 \
+#   --root_path ./dataset/Energy/ \
+#   --data_path load_forecasting.csv \
+#   --model_id 2_2_sincos \
+#   --model Autoformer \
+#   --data Energy \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 24 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 16 \
+#   --dec_in 16 \
+#   --c_out 16 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --embed sincos
 
 python3 -u run.py \
   --is_training 1 \
@@ -311,7 +311,7 @@ python3 -u run.py \
   --is_training 1 \
   --root_path ./dataset/Energy/ \
   --data_path load_forecasting.csv \
-  --model_id 4_1train_1val_3test \
+  --model_id 6_2factor \
   --model Autoformer \
   --data Energy \
   --features M \
@@ -320,13 +320,49 @@ python3 -u run.py \
   --pred_len 24 \
   --e_layers 2 \
   --d_layers 1 \
-  --factor 3 \
+  --factor 2 \
   --enc_in 16 \
   --dec_in 16 \
   --c_out 16 \
   --des 'Exp' \
-  --itr 1 \
-  --train_yrs 1 \
-  --val_yrs 1 \
-  --test_yrs 3
+  --itr 1
 
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 6_1factor \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 1 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1
+
+python3 -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/Energy/ \
+  --data_path load_forecasting.csv \
+  --model_id 6_5factor \
+  --model Autoformer \
+  --data Energy \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 5 \
+  --enc_in 16 \
+  --dec_in 16 \
+  --c_out 16 \
+  --des 'Exp' \
+  --itr 1
